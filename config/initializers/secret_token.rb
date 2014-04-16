4da25c0e30878c34b4d28336b07282344cf8c943
Gallagher Polyn <gallagher.polyn@gmail.com>
2014-04-16 13:50:52 -0400

    initial rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capistrano3RailsTemplate::Application.config.secret_key_base = '5c03882241c6218553e0f41cb9912cbdf0c3f655c09231784b205e8e2d509d5c0d2c06ed10cd976d448a6c3c5a46e8f17b6a56e47a8ed0baec032eda9cda9800'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capistrano3RailsTemplate::Application.config.session_store :cookie_store, key: '_capistrano-3-rails-template_session'
